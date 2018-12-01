be14011106bd0ef0f1a8a0c48af2167cd5c65cde
Aurélien Delogu <dev@dreamysource.fr>
2018-12-01 18:11:45 +0100

    Minor fixes

--- 2018/day01.part2.php
+++ 2018/day01.part2.php
@@ -15,4 +15,5 @@ echo $resolve(['+1', '-1']), "\n";
 echo $resolve(['+3', '+3', '+4', '-2', '-4']), "\n";
 echo $resolve(['-6', '+3', '+8', '+5', '-6']), "\n";
 echo $resolve(['+7', '+7', '-2', '-7', '-4']), "\n";
-echo $resolve(file('day01.puzzle.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)), "\n";
+
+echo $resolve(file(__DIR__ . '/day01.puzzle.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)), "\n";
